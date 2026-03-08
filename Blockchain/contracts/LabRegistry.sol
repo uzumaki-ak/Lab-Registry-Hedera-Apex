e1db046eec1db4c89ea9d5b04b41a3de568b743f
Crimson-Ox <your-no-reply-email@users.noreply.github.com>
2026-03-08 11:26:31 +0100

    feat:V4.3 Hardened Master

--- Blockchain/contracts/LabRegistry.sol
+++ Blockchain/contracts/LabRegistry.sol
@@ -1,21 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.23; //V4.2
+pragma solidity 0.8.23;
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract LabRegistry is Ownable {
+contract LabRegistry is Ownable, Pausable {
     uint256 public anchorFee = 1e8; 
     bool public automationEnabled;
     mapping(address => bool) public authorizedAgents;
-
     struct LabReport { string results; string tech; uint256 time; address patient; }
     mapping(uint256 => LabReport) private labReports;
     event ReportAnchored(uint256 indexed id, address indexed patient, string tech, uint256 time);
 
     constructor(address _owner) Ownable(_owner) {}
 
-    function setAutomation(bool _status) external onlyOwner { automationEnabled = _status; }
+    function pause() external onlyOwner { _pause(); }
+    function unpause() external onlyOwner { _unpause(); }
 
-    function addReport(uint256 _id, string memory _res, string memory _tech, address _pat) public payable {
+    function addReport(uint256 _id, string memory _res, string memory _tech, address _pat) public payable whenNotPaused {
         bool isAuth = (msg.sender == owner() || (automationEnabled && authorizedAgents[msg.sender]));
         require(isAuth, "Not Authorized");
         require(msg.value >= anchorFee, "Fee Required");
@@ -23,4 +24,3 @@ contract LabRegistry is Ownable {
         emit ReportAnchored(_id, _pat, _tech, block.timestamp);
     }
 }
-    
